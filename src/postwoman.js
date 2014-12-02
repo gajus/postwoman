620d8f86df89e284ec9f75f17186e76574f48e5a
Gajus Kuizinas <gk@anuary.com>
2014-12-02 19:36:49 +0200

    Validate and extract methods.

--- src/postwoman.js
+++ src/postwoman.js
@@ -1,6 +1,31 @@
 var Postcode = require('postcode'),
     Postwoman = {};
 
+Postwoman.match = function (input) {
+    var regex,
+        matches;
+
+    // @see http://regexlib.com/REDetails.aspx?regexp_id=260
+    regex = /([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]?\s?[0-9][ABD-HJLN-UW-Z]{2})/ig;
+
+    matches = input.match(regex);
+
+    matches = matches || [];
+
+    matches = matches.filter(Postwoman.validate);
+    matches = matches.map(Postwoman.normalise);
+
+    return matches;
+};
+
+Postwoman.validate = function (input) {
+    var postcode;
+
+    postcode = new Postcode(input);
+
+    return postcode.valid();
+}
+
 Postwoman.normalise = function (input) {
     var postcode;
 
--- tests/postwoman.js
+++ tests/postwoman.js
@@ -2,6 +2,26 @@ var expect = require('chai').expect,
     Postwoman = require('../src/postwoman.js');
 
 describe('Postwoman', function () {
+    describe('.match()', function () {
+        it('extracts valid postcode from a string', function () {
+            expect(Postwoman.match('PE12 9PP; DN3 6GB')).to.deep.equal(['PE12 9PP', 'DN3 6GB']);
+        });
+        it('normalises extracted postcodes', function () {
+            expect(Postwoman.match('PE129PP; DN36GB')).to.deep.equal(['PE12 9PP', 'DN3 6GB']);
+        });
+    });
+    describe('.validate()', function () {
+        describe('when postcode is valid', function () {
+            it('returns true', function () {
+                expect(Postwoman.validate('PE12 9PP')).to.true;
+            });
+        });
+        describe('when postcode is invalid', function () {
+            it('returns false', function () {
+                expect(Postwoman.validate('PE12 APP')).to.false;
+            });
+        });
+    });
     describe('.normalise()', function () {
         it('separates outward and inward code with a space', function () {
             expect(Postwoman.normalise('PE129PP')).to.equal('PE12 9PP');
