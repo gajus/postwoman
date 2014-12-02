e38ab2244b0843f4955664aec9569ce4e4753c89
Gajus Kuizinas <gk@anuary.com>
2014-12-02 19:00:51 +0200

    Using postcode to normalise postcode.

--- src/postwoman.js
+++ src/postwoman.js
@@ -1,5 +1,17 @@
-var Postwoman = {};
+var Postcode = require('postcode'),
+    Postwoman = {};
 
+Postwoman.normalise = function (input) {
+    var postcode;
 
+    postcode = new Postcode(input);
+    postcode = postcode.normalise();
+
+    if (!postcode) {
+        throw new Error('Cannot normalise invalid postcode ("' + input + '").')
+    }
+
+    return postcode;
+};
 
 module.exports = Postwoman;
--- tests/postwoman.js
+++ tests/postwoman.js
@@ -1,4 +1,18 @@
 var expect = require('chai').expect,
     Postwoman = require('../src/postwoman.js');
 
-describe('')
+describe('Postwoman', function () {
+    describe('.normalise()', function () {
+        it('separates outward and inward code with a space', function () {
+            expect(Postwoman.normalise('PE129PP')).to.equal('PE12 9PP');
+        });
+        it('makes postcode to uppercase', function () {
+            expect(Postwoman.normalise('pe129pp')).to.equal('PE12 9PP');
+        });
+        it('throws an error if postcode is invalid', function () {
+            expect(function () {
+                Postwoman.normalise('foobar');
+            }).to.throw(Error, 'Cannot normalise invalid postcode ("foobar").');
+        });
+    });
+});
